af19b934d61dbaead607b4744b76be3318b713b6
Luan Apollo Portes Rodrigues <142546826+p0lliinh0@users.noreply.github.com>
2025-02-20 00:19:46 +0000

    Aula-1925

---
+++ next-app/app/api/tarefas/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({
+        mensagem: "Olá, Next!"
+    }); 
+    
+}
+
+
+export async function POST (){
+    return NextResponse.json({
+        mensagem: "Olá, Next - POST!!"
+    });
+}
